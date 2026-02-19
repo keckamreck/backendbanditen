10addf3cced281be123c20a25d00e12a9ed46abc
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-02-19 13:36:56 +0100

    Specify FormData type

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,16 +4,16 @@ import { redirect } from 'next/navigation'
 import styles from './page.module.css'
 
 function Login() {
-  function verify(request){
+  function verifyData(formData: FormData){
     alert(`Redirecting...`);
     redirect('/');
   }
 
   return (
-    <form className={styles.container} action={verify}>
+    <form className={styles.container} action={verifyData}>
       <h1>BackendBanditen</h1>
       <input type="text" placeholder="Username" name="username" />
-      <input type="password" placeholder="Password" sname="password" />
+      <input type="password" placeholder="Password" name="password" />
       <button type="submit">Login</button>
     </form>
   );
