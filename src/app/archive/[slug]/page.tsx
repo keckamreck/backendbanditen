f0c273e90178ee759ef8f466557d5ea53795a6a0
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-02-27 21:17:25 +0100

    Add useState function to delete all tasks

--- src/app/archive/[slug]/page.tsx
+++ src/app/archive/[slug]/page.tsx
@@ -1,23 +1,36 @@
-import { getGroup } from '@/app/_models/group';
+"use client";
+
+import { Group, getGroup } from '@/app/_models/group';
 import { renderTask } from '@/app/_components/card';
+import { DeleteButton } from '@/app/_components/button';
+import { useParams } from 'next/navigation';
+import { useState } from 'react';
 import styles from './page.module.css'
 
-export default async function ArchiveGroup({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  const slug = (await params).slug;
+export default function ArchivePage()
+{
+  const params = useParams<{ slug: string }>();
+  const slug = params.slug;
   const group = getGroup(slug);
-  const listTasks = group.entries.map(task => {
-    return renderTask(task);
-  });
+  const [tasks, deleteTasks] = useState(getTasks(group));
+
+  function handleDelete()
+  {
+    deleteTasks([]);
+  }
 
   return (
     <>
       <h1>Group: {group.title}</h1>
-      {listTasks}
+      {tasks}
+      <DeleteButton onClick={handleDelete}/>
     </>
   );
 }
 
+function getTasks(group: Group)
+{
+  return group.entries.map(task => {
+    return renderTask(task);
+  });
+}
