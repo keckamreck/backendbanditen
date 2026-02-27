613709d1b2ac5ebe1e3fd6c9166e578dc3c0f8cb
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-02-27 21:20:28 +0100

    Upload changes

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,17 +1,15 @@
-import { Task } from "../_models/task";
-import { getPriority } from "../_lib/priority";
+import { Task } from '@/app/_models/task';
+import { getPriority } from '@/app/_lib/priority';
 import styles from './card.module.css'
 
 export function renderTask(task: Task) {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.card}>
-          <h1>{task.title}</h1>
-          <h4>{task.id}</h4>
-          <h3>{getPriority(task.priority)} Priority</h3>
-        </div>
+    <div key={task.id} className={styles.container}>
+      <div className={styles.card}>
+        <h1>{task.title}</h1>
+        <h4>{task.id}</h4>
+        <h3>{getPriority(task.priority)} Priority</h3>
       </div>
-    </>
+    </div>
   );
 }
