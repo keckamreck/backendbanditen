fa0a0dc39e7ed722527eb5f5ec79925e49573ae1
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-02-28 14:12:49 +0100

    Add getColor function for Priority

--- src/app/_lib/priority.tsx
+++ src/app/_lib/priority.tsx
@@ -1,5 +1,16 @@
-import { Priority } from "../_models/task";
+import { Priority } from "@/app/_models/task";
 
 export function getPriority(id: number) {
   return Priority[id];
+}
+export function getColor(priority: Priority)
+{
+  switch(priority) {
+    case Priority.High:
+      return "red";
+    case Priority.Medium:
+      return "orange";
+    case Priority.Low:
+      return "green";
+  }
 }
