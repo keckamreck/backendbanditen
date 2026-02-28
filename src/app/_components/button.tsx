ff756a19826c48551d3e59718c9bf86ae45c3677
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-02-28 12:50:52 +0100

    Add FontAwesome icons

--- src/app/_components/button.tsx
+++ src/app/_components/button.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { ButtonHTMLAttributes } from "react";
+import styles from './button.module.css'
 
 export function DeleteButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button id="delete-button" type="button" onClick={props.onClick}>Delete</button>
+    <button id="delete-button" type="button" onClick={props.onClick} className={styles.button}>
+      <FontAwesomeIcon icon={faTrash}/>
+    </button>
   );
 }
