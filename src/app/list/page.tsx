5385b0da90d43cd7330cb806e54c963d7c11261b
Miriwiewowas <miriwie06@gmail.com>
2026-03-05 09:01:07 +0100

    inititial display

---
+++ src/app/list/page.tsx
@@ -0,0 +1,23 @@
+import { getList } from "@/app/_models/list";
+import { getTasks } from "@/app/_models/list";
+import { TaskCard } from "@/app/_components/card";
+import styles from "./page.module.css";
+
+function List({ ListId }: { ListId: number }) {
+  const list = getList(ListId);
+  const tasks = getTasks(ListId);
+  return (
+    <div className={styles.list}>
+      <h1>{list.title}</h1>
+      <div className={styles.tasks}>
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default function Page() {
+  return <List ListId={1} />;
+}
