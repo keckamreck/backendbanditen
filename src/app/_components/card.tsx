28209296dc3069fe976060614469418a73da547c
Miriwiewowas <miriwie06@gmail.com>
2026-03-05 16:32:07 +0100

    changed card view

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,7 +1,11 @@
-import { PriorityBadge } from './badge';
-import { Task } from '@/app/_models/task';
-import { getPriority } from '@/app/_lib/priority';
-import styles from './card.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { PriorityBadge } from "./badge";
+import { Task } from "@/app/_models/task";
+import { getPriority } from "@/app/_lib/priority";
+import styles from "./card.module.css";
 
 export interface TaskProps {
   task: Task;
@@ -11,14 +15,18 @@ export function TaskCard(props: TaskProps) {
   const task = props.task;
   return (
     <div className={styles.container}>
+      <div className={styles.check}>
+        <FontAwesomeIcon icon={faCircle} />
+        <FontAwesomeIcon icon={faCheckCircle} />
+      </div>
       <div className={styles.card}>
-        <div className={styles.row}>
-          <h1>{task.title}</h1>
-          <h4>{task.id}</h4>
-        </div>
+        <h1>{task.title}</h1>
+        <button className={styles.buttonEdit}>
+          <FontAwesomeIcon icon={faPencil} />
+        </button>
         <div className={styles.row}>
           <h3>Fällig am {task.deadline?.toLocaleDateString()}</h3>
-          <PriorityBadge priority={task.priority}/> 
+          <PriorityBadge priority={task.priority} />
         </div>
       </div>
     </div>
