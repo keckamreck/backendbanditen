38fc7169913728248798338e22e0c4eda52814d1
Carolin Matz <carolin.matz@burda.com>
2026-03-10 12:01:00 +0100

    added dropdown component and buttons for editor component

---
+++ src/app/_components/buttonsEditor.tsx
@@ -0,0 +1,84 @@
+import styles from "./buttonsEditor.module.css";
+import { Priority } from "@/app/_models/task";
+import { Dispatch, SetStateAction, useState } from "react";
+
+type PropsPriority = {
+  className?: string;
+  action: Dispatch<SetStateAction<Priority>>;
+  defaultValue: Priority;
+};
+type PropsButton = {
+  className?: string;
+  action: () => void;
+  disabled?: boolean;
+  task: "save" | "delete";
+};
+
+export function PriorityButton({
+  className,
+  action,
+  defaultValue,
+}: PropsPriority) {
+  const [lowActive, setLowActive] = useState<"lowFocus" | "">(
+    defaultValue === Priority.Low ? "lowFocus" : "",
+  );
+  const [mediumActive, setMediumActive] = useState<"mediumFocus" | "">(
+    defaultValue === Priority.Medium ? "mediumFocus" : "",
+  );
+  const [highActive, setHighActive] = useState<"highFocus" | "">(
+    defaultValue === Priority.High ? "highFocus" : "",
+  );
+
+  function setPriority(priority: string): void {
+    if (priority === "Low") {
+      action(Priority.Low);
+      setLowActive("lowFocus");
+      setMediumActive("");
+      setHighActive("");
+    } else if (priority === "Medium") {
+      action(Priority.Medium);
+      setLowActive("");
+      setMediumActive("mediumFocus");
+      setHighActive("");
+    } else if (priority === "High") {
+      action(Priority.High);
+      setLowActive("");
+      setMediumActive("");
+      setHighActive("highFocus");
+    }
+  }
+
+  return (
+    <div className={className}>
+      <button
+        className={`${styles.low} ${styles.priority} ${styles[lowActive]}`}
+        onClick={(): void => setPriority("Low")}
+      >
+        niedrig
+      </button>
+      <button
+        className={`${styles.medium} ${styles.priority} ${styles[mediumActive]}`}
+        onClick={(): void => setPriority("Medium")}
+      >
+        mittel
+      </button>
+      <button
+        className={`${styles.high} ${styles.priority} ${styles[highActive]}`}
+        onClick={(): void => setPriority("High")}
+      >
+        hoch
+      </button>
+    </div>
+  );
+}
+export function Button({ className, action, disabled, task }: PropsButton) {
+  return (
+    <button
+      className={`${styles[task]} ${styles.button} ${className}`}
+      onClick={(): void => action()}
+      disabled={disabled}
+    >
+      {task === "save" ? "speichern" : "löschen"}
+    </button>
+  );
+}
