67bca11d8e8fa72106f09080d509eac295d99762
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-03-11 10:54:30 +0100

    Include new done button function

--- src/app/archive/[id]/page.tsx
+++ src/app/archive/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { getTaskofList } from "@/app/_models/function";
-import { getList } from "@/app/_models/function";
+import { getList, getTaskofList } from "@/app/_models/function";
 import { Task } from "@/app/_models/task";
 import { TaskCard } from "@/app/_components/TaskCard";
 import { ArchiveButton, DeleteButton } from "@/app/_components/button";
@@ -14,10 +13,18 @@ export default function ArchivePage() {
   const params = useParams<{ id: string }>();
   const id = Number(params.id);
   const list = getList(id);
-  const [tasks, deleteTasks] = useState(getTaskofList(list.id));
+  const [tasks, handleTasks] = useState(getTaskofList(list.id));
 
   function handleDelete() {
-    deleteTasks([]);
+    handleTasks([]);
+  }
+
+  function handleDone(id: number) {
+    handleTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task,
+      ),
+    );
   }
 
   return (
@@ -25,22 +32,22 @@ export default function ArchivePage() {
       <Header title={list.title}>
         <ArchiveButton onClick={handleDelete} />
       </Header>
-      {showTasks(tasks)}
+      <div className={styles.tasks}>
+        {showTasks(tasks, handleDone)}
+      </div>
       <DeleteButton className={styles.buttonDelete} onClick={handleDelete} />
     </>
   );
 }
 
-function showTasks(tasks: Task[]) {
+function showTasks(tasks: Task[], handler: any) {
   return tasks.map((task) => {
     return (
       <TaskCard
         key={task.id}
         task={task}
         onPencilClick={() => console.log("Pencil clicked")}
-        onDoneClick={() =>
-          (task.done = !task.done) && console.log("Done clicked " + task.done)
-        }
+        onDoneClick={() => { handler(task.id) }}
       />
     );
   });
