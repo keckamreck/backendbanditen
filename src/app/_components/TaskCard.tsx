a144df4a7bec2fd5ff4e5bd6742807cd2dd661c3
Miriwiewowas <miriwie06@gmail.com>
2026-03-11 12:31:37 +0100

    better date and time on the task card

--- src/app/_components/TaskCard.tsx
+++ src/app/_components/TaskCard.tsx
@@ -16,6 +16,22 @@ export interface TaskProps {
 
 export function TaskCard(props: TaskProps) {
   const task = props.task;
+  const deadlineDate = task.deadline
+    ? task.deadline.toLocaleDateString("de-DE", {
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+      })
+    : null;
+  const deadlineTime = task.deadline
+    ? task.deadline.toLocaleTimeString("de-DE", {
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+    : null;
+  const deadline =
+    deadlineDate && deadlineTime ? `${deadlineDate}, ${deadlineTime}` : null;
   return (
     <div className={styles.container}>
       <div className={styles.check}>
@@ -33,7 +49,8 @@ export function TaskCard(props: TaskProps) {
           <FontAwesomeIcon icon={faPencil} />
         </button>
         <div className={styles.row}>
-          <h3>Fällig am {task.deadline?.toLocaleDateString()}</h3>
+          {/* <h3>{(task.deadline?.getMonth() + 1).toString()}</h3> */}
+          <h3>{deadline}</h3>
           <PriorityBadge priority={task.priority} />
         </div>
       </div>
