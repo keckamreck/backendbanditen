60f264079adf31f7884db0191d0c09f87c0ae287
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-07-04 12:36:46 +0200

    Implement users actions

---
+++ backend/src/repositories/users.ts
@@ -0,0 +1,46 @@
+import { user as users } from "../db/schema.js";
+import { db } from "./db.js";
+import { eq } from 'drizzle-orm';
+
+export interface User {
+  id: string
+  username: string,
+  password: string,
+  email: string
+}
+
+export async function createData(user: User) {
+  await db.insert(users)
+  .values({
+    id: crypto.randomUUID(),
+    username: user.username,
+    password: user.password,
+    email: user.email,
+  });
+}
+
+export async function readData(id: string) {
+  return await db.select()
+  .from(users)
+  .where(eq(users.id, id));
+}
+
+export async function readAllData() {
+  return await db.select()
+  .from(users)
+}
+
+export async function updateData(user: User) {
+  await db.update(users)
+  .set({
+    username: user.username,
+    password: user.password,
+    email: user.email,
+  })
+  .where(eq(users.id, user.id));
+}
+
+export async function deleteData(id: string) {
+  await db.delete(users)
+  .where(eq(users.id, id));
+}
