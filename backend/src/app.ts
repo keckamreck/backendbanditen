e5968b8bb4f7faae7d18cb4a54d0fcc1e5d27465
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-07-05 15:23:06 +0200

    Add cors and better-auth configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,19 @@
 //@ts-ignore
 import express from "express";
+import cors from 'cors';
 import { router as userRouter } from "./routers/user-router.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth.js";
 
 const app = express();
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
+app.all("/auth/*splat", cors(corsOptions), toNodeHandler(auth));
 app.use(express.json());
 app.use("/users", userRouter);
 
---
+++ backend/src/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+//import { username } from "better-auth/plugins"
+import { db } from "./repositories/db.js";
+import * as schema from "./db/schema.js";
+
+export const auth = betterAuth({
+    disableTrustedOriginsCors: true,
+    trustedOrigins: ['http://localhost:3000'],
+    baseURL: "http://localhost:8097/auth",
+    database: drizzleAdapter(db, {
+        provider: "pg", // or "pg" or "mysql"
+        schema: { 
+            ...schema, 
+            user: schema.user,
+        }, 
+    }),
+    emailAndPassword: { 
+        enabled: true, 
+    },
+});
