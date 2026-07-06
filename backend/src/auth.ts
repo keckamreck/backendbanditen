df24d11fba0c18c5e9aec5002e5179e652cc35da
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-07-06 09:18:13 +0200

    Update better-uth configuration

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,8 +1,7 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, uuid } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-//import { username } from "better-auth/plugins"
+import { username } from "better-auth/plugins"
 import { db } from "./repositories/db.js";
-import * as schema from "./db/schema.js";
 
 export const auth = betterAuth({
     disableTrustedOriginsCors: true,
@@ -10,12 +9,16 @@ export const auth = betterAuth({
     baseURL: "http://localhost:8097/auth",
     database: drizzleAdapter(db, {
         provider: "pg",
-        schema: { 
-            ...schema, 
-            user: schema.user,
-        }, 
     }),
+    advanced: {
+        database: {
+            generateId: "uuid"
+        }
+    },
     emailAndPassword: { 
         enabled: true, 
     },
+    plugins: [ 
+        username() 
+    ] 
 });
