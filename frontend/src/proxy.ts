1c721d149612e6400f3809619d7846b05a97af75
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-07-07 14:17:20 +0200

    Protect only matching routes

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -7,13 +7,18 @@ export default async function proxy(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const sessionCookie = getSessionCookie(req);
   const isPublic = publicRoutes.includes(path);
-  const isPrivate = !isPublic;
+  const isProtected = !isPublic;
 
-  if (isPrivate && !sessionCookie) {
+  if (isProtected && !sessionCookie) {
     return NextResponse.redirect(new URL('/login', req.nextUrl));
   } else if (isPublic && sessionCookie) {
     return NextResponse.redirect(new URL('/dashboard', req.nextUrl));
   }
  
   return NextResponse.next();
+}
+
+export const config = {
+  matcher:  ['/', '/login', '/register', '/dashboard', '/list/:path*',
+    '/createTask/:path*', '/editTask/:path*', '/archive/:path*'],
 }
