d968cbc868380d180bbd91ef5ca9a68a3ea289d6
dominikstengele <domi.stengele@outlook.de>
2026-07-09 22:26:32 +0200

    preview sollte wieder laufen.

--- backend/src/db/relations.ts
+++ backend/src/db/relations.ts
@@ -1,39 +1,39 @@
 import { relations } from "drizzle-orm/relations";
-import { user, list, category, task } from "./schema";
+import { user, list, category, task } from "./schema.js";
 
-export const listRelations = relations(list, ({one, many}) => ({
-	user: one(user, {
-		fields: [list.userId],
-		references: [user.id]
-	}),
-	category: one(category, {
-		fields: [list.categoryId],
-		references: [category.id]
-	}),
-	tasks: many(task),
+export const listRelations = relations(list, ({ one, many }) => ({
+  user: one(user, {
+    fields: [list.userId],
+    references: [user.id],
+  }),
+  category: one(category, {
+    fields: [list.categoryId],
+    references: [category.id],
+  }),
+  tasks: many(task),
 }));
 
-export const userRelations = relations(user, ({many}) => ({
-	lists: many(list),
-	tasks: many(task),
-	categories: many(category),
+export const userRelations = relations(user, ({ many }) => ({
+  lists: many(list),
+  tasks: many(task),
+  categories: many(category),
 }));
 
-export const categoryRelations = relations(category, ({one, many}) => ({
-	lists: many(list),
-	user: one(user, {
-		fields: [category.userId],
-		references: [user.id]
-	}),
+export const categoryRelations = relations(category, ({ one, many }) => ({
+  lists: many(list),
+  user: one(user, {
+    fields: [category.userId],
+    references: [user.id],
+  }),
 }));
 
-export const taskRelations = relations(task, ({one}) => ({
-	list: one(list, {
-		fields: [task.listId],
-		references: [list.id]
-	}),
-	user: one(user, {
-		fields: [task.userId],
-		references: [user.id]
-	}),
+export const taskRelations = relations(task, ({ one }) => ({
+  list: one(list, {
+    fields: [task.listId],
+    references: [list.id],
+  }),
+  user: one(user, {
+    fields: [task.userId],
+    references: [user.id],
+  }),
 }));
