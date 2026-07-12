58976d687bc873f21c33e5274b2e24891b2953e9
Carolin Matz <carolin.matz@burda.com>
2026-07-12 21:10:46 +0200

    added error handling system

---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,23 @@
+import { AppError } from "../errors/errors.js";
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandler(
+  error: unknown,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
+): void {
+  if (error instanceof AppError) {
+    response
+      .status(error.statusCode)
+      .send({ error: { code: error.code, message: error.message } });
+  } else {
+    console.error(error);
+    response.status(500).send({
+      error: {
+        code: "INTERNAL_SERVER_ERROR",
+        message: "An internal server error occurred",
+      },
+    });
+  }
+}
