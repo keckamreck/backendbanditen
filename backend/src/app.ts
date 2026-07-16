b56e8db6d2f98b679b36c3869e93dfc7e311a941
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-07-16 09:27:20 +0200

    Move usersRouter to wrapper

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from 'cors';
-import { router as usersRouter } from "./routers/users-router.js";
 import { router as userContextRouter } from "./routers/user-context-router.js";
 import {router as categoriesRouter} from "./routers/categories-router.js";
 import { toNodeHandler } from "better-auth/node";
@@ -17,7 +16,6 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.all("/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
-app.use("/users", usersRouter);
 app.use("/users/:userId", userContextRouter);
 app.use("/", categoriesRouter);
 
