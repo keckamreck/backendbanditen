7718323377b3ff0b83a913e60e58948a70893290
deadbeef-alo <loewenthal.adolf.a25@student.dhbw-karlsruhe.de>
2026-07-20 12:05:40 +0200

    Remove dev buttons

--- frontend/src/app/_components/logout.tsx
+++ frontend/src/app/_components/logout.tsx
@@ -2,20 +2,11 @@
 
 import { CloseButton, BurgerButton } from "@/app/_components/ButtonsIcon";
 import { authClient } from "../_lib/auth-client";
-import { getUserId } from "@/app/_api/users-api";
 import { useState } from "react";
 import styles from "./logout.module.css";
 import { redirect } from "next/navigation";
 
 export function Logout() {
-  async function handleUser() {
-    const userId = await getUserId();
-    prompt("User ID:", JSON.stringify(userId));
-  }
-  async function handleZong() {
-    const { data: session } = await authClient.getSession();
-    prompt("User data:", JSON.stringify(session, null, " "));
-  }
   async function handleLogout() {
     await authClient.signOut({
       fetchOptions: {
@@ -50,12 +41,6 @@ export function Logout() {
           <CloseButton onClick={handleClose} className={styles.closeButton} />
           <div className={styles.modalTitle}>Kontoverwaltung</div>
           <div className={styles.actionContainer}>
-            <button onClick={handleUser} className={styles.actionButton}>
-              UserID
-            </button>
-            <button onClick={handleZong} className={styles.actionButton}>
-              Zong
-            </button>
             <button onClick={handleLogout} className={styles.actionButton}>
               Ausloggen
             </button>
